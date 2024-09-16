c5044c28df4d154d091f3703a0beafe82636af77
tinCrispy <basilmarples@outlook.com>
2024-09-16 13:13:56 +0100

    Basils initial movement
    addforce.impulse

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpSpeed = 10;
+    public float moveSpeed = 2;
+    public Rigidbody2D rb;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded == true)
+        {
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.AddForce(Vector3.left * moveSpeed, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.AddForce(Vector3.right * moveSpeed, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Planet"))
+            isGrounded = true;
+            Debug.Log("grounded");
+
+    }
+}
