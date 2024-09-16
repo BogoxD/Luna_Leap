ed9972274a246b529b54dd4255b20373b66d9f48
BogoxD <91665508+BogoxD@users.noreply.github.com>
2024-09-16 14:55:10 +0100

    push

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,55 +4,72 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float jumpSpeed = 10;
-    public float moveSpeed = 2;
-    public Rigidbody2D rb;
-    private bool isGrounded;
+    [Header("Movement")]
+    public float jumpForce = 10;
+    public float moveForce = 2;
+    public float maxSpeed = 5f;
+
+    [Header("Rotation")]
+    public float playerMaxRotation = 20f;
+
+
+    private Rigidbody2D rb2d;
+    public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        OnMove();
+        OnJump();
+    }
+    private void OnMove()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        rb2d.AddForce(horizontal * moveForce * Vector2.right, ForceMode2D.Force);
+
+        //sets a max speed so the player doesn't accelerate to infinity
+        LimitVelocity();
+
+        //clamp rotation
+        ClampRotation();
+    }
+    private void OnJump()
+    {
         if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded == true)
         {
-            rb.AddForce(Vector3.up * jumpSpeed, ForceMode2D.Impulse);
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
         }
-
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.AddForce(Vector3.left * moveSpeed, ForceMode2D.Force);
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.AddForce(Vector3.right * moveSpeed, ForceMode2D.Force);
-        }
     }
+    private void ClampRotation()
+    {
+        Vector3 playerEulerAngles = transform.localEulerAngles;
 
-    void OnCollisionEnter(Collision collision)
+        playerEulerAngles.z = Mathf.Clamp(playerEulerAngles.z, -playerMaxRotation, playerMaxRotation);
+
+        transform.rotation = Quaternion.Euler(0, 0, playerEulerAngles.z);
+    }
+    private void LimitVelocity()
+    {
+        rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed), rb2d.velocity.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Planet"))
             isGrounded = true;
-            Debug.Log("grounded");
-
     }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-
         if (other.CompareTag("Pickup"))
         {
             Destroy(other.gameObject);
         }
-        
     }
-
-
 }
