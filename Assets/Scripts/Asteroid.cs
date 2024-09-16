4b4a7664afda933a9ded7e8ae434de83926ae623
BogoxD <91665508+BogoxD@users.noreply.github.com>
2024-09-16 10:22:58 +0100

    changes

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -6,6 +6,7 @@ public class Asteroid : MonoBehaviour
 {
     public float force;
     private Rigidbody2D rb2d;
+    private Vector2 forceDirection;
 
     private void Start()
     {
@@ -17,13 +18,18 @@ public class Asteroid : MonoBehaviour
     }
     private void Move()
     {
-        rb2d.AddForce((-Vector2.one * 0.5f) * force, ForceMode2D.Force);
+        forceDirection = -Vector2.one * Random.Range(0.1f, 1f);
+        rb2d.AddForce((forceDirection) * force, ForceMode2D.Force);
     }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Planet"))
         {
-
+            if(collision.gameObject.TryGetComponent(out Rigidbody2D rigidbody))
+            {
+                //add force to planet when hitting it
+                rigidbody.AddForce(forceDirection * 2f, ForceMode2D.Impulse);
+            }
         }
     }
 }
